6be52325297fd92ddbd809ff830981ce19c18b34
Edidiong <ephraimedy@gmail.com>
2023-11-23 17:56:55 +0100

    Started build of side panel

---
+++ tensor_chat/src/components/Chats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+/* This component will keep track of the conversation a user is having*/
+function Chats() {
+  return (
+    <div>Chats</div>
+  )
+}
+
+export default Chats
--- tensor_chat/src/components/SidePanel.js
+++ tensor_chat/src/components/SidePanel.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import "./myStyles.css";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { IconButton } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Chats from './Chats';
+
+
 function SidePanel() {
   return (
     <div className='side-panel'>
         {/* The header section of the side panel*/}
         <div className='sp-header'>
-            {/*The <IconButton> makes the account.. icon clickable*/}
-            <IconButton>
-                  <AccountCircleIcon/>
-            </IconButton>
-            <IconButton>
-                <PersonAddAltOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <GroupAddOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <AddCircleOutlineOutlinedIcon/>
-            </IconButton>
-            <IconButton>
-                <DarkModeOutlinedIcon/>
-            </IconButton>
+            {/*The <IconButton> makes the account.. icon clickable 
+            the account */ }
+            <div>
+                  <IconButton>
+                      <AccountCircleIcon />
+                  </IconButton>
+            </div>
+            
+            <div>
+                  <IconButton>
+                      <PersonAddAltOutlinedIcon />
+                  </IconButton>
+                  <IconButton>
+                      <GroupAddOutlinedIcon />
+                  </IconButton>
+                  <IconButton>
+                      <AddCircleOutlineOutlinedIcon />
+                  </IconButton>
+                  <IconButton>
+                      <DarkModeOutlinedIcon />
+                  </IconButton>
 
+            </div>
+            
         </div>
 
         {/* The search section of the side panel*/}
-        <div className='sp-search'>search</div>
+        <div className='sp-search'>
+            <IconButton>
+                <SearchOutlinedIcon />
+            </IconButton>
+            <input placeholder='Search' className='search-box'></input>
+        </div>
 
         {/* The chat section of the side panel*/}
-        <div className='sp-chatsection'>chat section</div>
+        <div className='sp-chatsection'> <Chats/>  </div>
     </div>
     
   )
