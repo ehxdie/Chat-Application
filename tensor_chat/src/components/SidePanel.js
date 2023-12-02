f49d4e23fd57d650d0cfae15e48045f65cd682d4
Edidiong <ephraimedy@gmail.com>
2023-12-02 16:37:33 +0100

    Implemented dark mode on the sidepanel

--- tensor_chat/src/components/SidePanel.js
+++ tensor_chat/src/components/SidePanel.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./myStyles.css";
-import { IconButton } from '@mui/material';
+import { IconButton, toggleButtonClasses } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 import Chats from './Chats';
 import { useState } from 'react';
 
@@ -47,20 +48,27 @@ function SidePanel() {
         )
     })
 
-   
+   /* Setting up toogle button to change to dark mode */
+   const [theme, setTheme] = useState(true);
+
+   const handleClick = () => {
+       setTheme(!theme);
+   };
+
     
   
   return (
     <div className='side-panel'>
         {/* The header section of the side panel*/}
-        <div className='sp-header'>
+
+        <div className={"sp-header" + (theme? "": "-dark")}>
             {/*The <IconButton> makes the account.. icon clickable 
             the account */ }
             <div>
                   <IconButton onClick={() => {
                       navigate("/ChatArea")
                   }}>
-                      <AccountCircleIcon />
+                      <AccountCircleIcon className={theme? "icons": "icons-dark"}/>
                   </IconButton>
             </div>
             
@@ -68,22 +76,20 @@ function SidePanel() {
                   <IconButton onClick={() => {
                       navigate("/UserList")
                   }} >
-                      <PersonAddAltOutlinedIcon />
+                      <PersonAddAltOutlinedIcon className={theme ? "icons" : "icons-dark"} />
                   </IconButton>
                   <IconButton onClick={() => {
                       navigate("/GroupList")
                   }} >
-                      <GroupAddOutlinedIcon />
+                      <GroupAddOutlinedIcon className={theme ? "icons" : "icons-dark"} />
                   </IconButton>
                   <IconButton onClick={() => {
                       navigate("/Groups")
                   }}>
-                      <AddCircleOutlineOutlinedIcon />
+                      <AddCircleOutlineOutlinedIcon className={theme ? "icons" : "icons-dark"} />
                   </IconButton>
-                  <IconButton onClick={()=> {
-
-                  }}>
-                      <DarkModeOutlinedIcon />
+                  <IconButton onClick={handleClick}>
+                      {theme && <DarkModeOutlinedIcon />} {!theme &&  <WbSunnyOutlinedIcon className="icons-dark"/>}
                   </IconButton>
 
             </div>
@@ -91,15 +97,15 @@ function SidePanel() {
         </div>
 
         {/* The search section of the side panel*/}
-        <div className='sp-search'>
+          <div className={"sp-search" + (theme ? "" : "-dark")}>
             <IconButton>
-                <SearchOutlinedIcon />
+                  <SearchOutlinedIcon className={theme ? "icons" : "icons-dark"} />
             </IconButton>
             <input placeholder='Search' className='search-box'></input>
         </div>
 
         {/* The chat section of the side panel*/}
-          <div className='sp-chatsection' >{Chat}</div>
+          <div className={"sp-chatsection" + (theme ? "" : "-dark")} >{Chat}</div>
     </div>
     
   )
